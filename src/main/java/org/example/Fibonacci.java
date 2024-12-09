5911ee1e76110ecbe83194de4588d1d442cd4f06
Jieyu Zheng <zheng.jiey@northeastern.edu>
2024-12-09 15:52:31 -0500

    final

--- src/main/java/org/example/Fibonacci.java
+++ src/main/java/org/example/Fibonacci.java
@@ -11,7 +11,15 @@ public class Fibonacci {
      * else <code>fibN(n-1)</code> + <code>fibN(n-2)</code>.
      */
     public long fibN(int n) {
-        return -1L;
+        if( n < 0 || n >= memo.length){
+       //throw new IllegalArgumentException(" 0 <= n <= 92 ");
+       return -1;
+        }
+          if (memo[n] != -1) {
+             return memo[n];
+          }
+          memo[n] = fibN(n - 1) + fibN(n - 2);
+          return memo[n];
     }
 
     /**
@@ -20,12 +28,30 @@ public class Fibonacci {
      * @param n the index into the Fibonacci series. <code>n</code> is positive and less than 93.
      * @return the sum of the Fibonacci numbers 0 through n.
      */
-    public long sumN(int n) {
-         return -1L;
-    }
+         public long sumN(int n) {
+            if (n < 0 || n >= memo.length) {
+                // throw new IllegalArgumentException("n must be between 0 and 92 inclusive");
+               return -1;
+            }
+            long sum = 0;
+            for (int i = 0; i <= n; i++) {
+                sum += fibN(i);
+            }
+            return sum;
+        }
+    
 
     public Fibonacci() {
+      memo[0] = 0;
+      memo[1] = 1;
+      for (int i = 2; i < memo.length; i++){
+        memo[i] = -1;
+      }
     }
 
+
     private final long[] memo = new long[93];
+
+    
 }
+
